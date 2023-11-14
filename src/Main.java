30c85aadd941a31ec3128dcf813ea00edbc52bc6
kannon herbert <kannonherbert@kannons-MacBook-Pro.local>
2023-11-13 18:15:45 -0800

    Herbert - Initialized Triangle class, and overode the abstract methods using herons formula for area. I then created new shape objects within the main clas.

--- src/Main.java
+++ src/Main.java
@@ -19,9 +19,9 @@ public class Main {
         shapes.add(new Rectangle(10.0, 40.0));
         shapes.add(new Rectangle(20.0, 20.0));
         shapes.add(new Circle(50.0));
-//        shapes.add(new Circle(100.0));
-//        shapes.add(new Triangle(3,4,5));
-//        shapes.add(new Triangle(7,9,13));
+        shapes.add(new Circle(100.0));
+        shapes.add(new Triangle(3,4,5));
+        shapes.add(new Triangle(7,9,13));
     }
 
     public static void display(ArrayList<Shape> shapes) {
--- src/Triangle.java
+++ src/Triangle.java
@@ -2,4 +2,37 @@
  * Represents a triangle
  */
 public class Triangle extends Shape {
+
+    private double sideOne;
+    private double sideTwo;
+    private double sideThree;
+
+    public Triangle(int sideOne, int sideTwo, int sideThree) {
+        this.sideOne = sideOne;
+        this.sideTwo = sideTwo;
+        this.sideThree = sideThree;
+    }
+
+    public double getSideOne() {
+        return sideOne;
+    }
+
+    public double getSideTwo() {
+        return sideTwo;
+    }
+
+    public double getSideThree() {
+        return sideThree;
+    }
+
+    @Override
+    public double computeArea() {
+        double p = (sideOne + sideTwo + sideThree) / 2;
+        return Math.sqrt(p * (p - sideOne) * (p - sideTwo) * (p - sideThree));
+    }
+
+    @Override
+    public double computePerimeter() {
+        return sideOne + sideTwo + sideThree;
+    }
 }
